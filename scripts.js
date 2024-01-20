1f69cf7a054bb156bdf87fd04afc4c2ab1c56d10
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-01-21 00:27:43 +0530

    Update the code to use an external JavaScript file. (generated by blackbox.ai )

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const passwordBOx = document.getElementById("password");
+const lengths = 6;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "1234567890";
+const symbols = "~!@#$%^&*_+-=?/><";
+
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCaseCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(lengths > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBOx.value = "password";
+}
