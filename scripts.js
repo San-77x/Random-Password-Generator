32522f29bfe880c02dfe1a1cc5707c9fd4fd9025
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-01-22 23:27:16 +0530

    Added javascript to copy the generated password

--- scripts.js
+++ scripts.js
@@ -1,22 +1,29 @@
 const passwordBOx = document.getElementById("password");
 const lengths = 6;
 
+
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "1234567890";
 const symbols = "~!@#$%^&*_+-=?/><";
-
 const allChars = upperCase + lowerCase + numbers + symbols;
+
 
 function createPassword() {
     let password = "";
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCaseCase.length)];
+    password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += numbers[Math.floor(Math.random() * numbers.length)];
     password += symbols[Math.floor(Math.random() * symbols.length)];
 
     while(lengths > password.length) {
         password += allChars[Math.floor(Math.random() * allChars.length)];
     }
-    return "password"
+    passwordBOx.value = pass
 }
+
+
+const button = document.getElementById("gen");
+
+button.onclick = createPassword
+
